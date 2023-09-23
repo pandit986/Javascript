f18b444554007580e2157f4387dd5b82798c6f5b
Abhishek Pandit <abhishekpandit986@gmail.com>
2023-09-23 08:07:24 +0000

    dataType

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,1 +1,55 @@
-const accountId = 9866888;
+const accountId = 9866888
+let accountEmail = "abhishekpandit986@gmail.com"
+var accountPassword = "1234567"
+accountCity = "Jaipur" // Not good Pratice //global scope
+let accountState;
+// accountId = 4  // not allowed 
+// prefer not to use var 
+//let have a block scope 
+
+accountEmail = "abc@gmail.com"
+accountPassword = "111122222"
+accountCity = "Delhi"
+accountPassword = "1234567"
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
+
+
+{
+    console.log(accountCity);
+}
+
+
+var a = 10 
+const b = 11 
+let c = 122
+
+const printing = () => {
+    console.log(a)
+    console.log(b)
+    console.log(c)
+}
+
+// printing()
+
+// for (let index = 0; index < 1; index++) {
+//     console.log(a)
+//     console.log(b)
+//     console.log(c)
+//     var i = index
+//     const k = index
+    
+// }
+
+// console.log(i)
+// console.log(k);
+
+{
+    let ans = 11
+    const printing = () => {
+        console.log(ans)
+        
+    }
+    
+    printing()
+}
---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,30 @@
+"use stricts"; //treate all Js code as newer version
+
+//alert(3+3) // we are using nodejs , not browser
+
+console.log(3+3)
+
+let name = "Abhishek"
+let age  = 18
+let isLoggedIn = false
+
+//primative datatype  -> type 
+
+// number => range 2 to power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => stand alone value
+// undefine =>  value is not assign yet
+// symbol => unique
+let log;
+console.log(log);
+
+let ans = null //ans is enpty
+console.log(ans);
+
+
+
+//object
+console.log(typeof ans); // type object 
+console.log(typeof undefined) // type is undefine
