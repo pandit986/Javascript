562f6db0f9fe3a7744e3e3bed275d49703efe18c
Abhishek Pandit <abhishekpandit986@gmail.com>
2023-09-24 17:44:42 +0000

    object

---
+++ 02_basics/05_object.js
@@ -0,0 +1,24 @@
+//destruction
+
+const course = {
+  coursename: "js in hindi",
+  price: "999",
+  courseInstructor: "Hitesh Sir",
+};
+
+const { price, courseInstructor: Teacher, coursename } = course;
+
+console.log(Teacher);
+
+// const navbar = ({company}) => {
+
+// }
+// navbar(company = "Abhishek")
+
+//Api Concept
+// {
+//     "name":"abhishek",
+//     "coursename":"js in hindi"
+// }
+
+[{}, {}];
