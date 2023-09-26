2da1534f3833bb3c5f43658194663fff832dd222
Abhishek Pandit <abhishekpandit986@gmail.com>
2023-09-26 12:34:13 +0000

    for each

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -1,32 +1,32 @@
 // for Each Loop
-const coding = ["js","ruby","java","python","cpp"]
+const coding = ["js", "ruby", "java", "python", "cpp"];
 
-coding.forEach((item,index,arr)=>{
-console.log(item);
-console.log(index);
-console.log(arr);
-})
+coding.forEach((item, index, arr) => {
+  console.log(item);
+  console.log(index);
+  console.log(arr);
+});
 
 const mycoding = [
-    {
-    languageName:"javascript",
-    languageFileName:"js",
-    },
-    {
-    languageName:"python",
-    languageFileName:"py",
-    },
-    {
-    languageName:"c++",
-    languageFileName:"cpp",
-    },
-    {
-    languageName:"java",
-    languageFileName:"java",
-    },
-]
+  {
+    languageName: "javascript",
+    languageFileName: "js",
+  },
+  {
+    languageName: "python",
+    languageFileName: "py",
+  },
+  {
+    languageName: "c++",
+    languageFileName: "cpp",
+  },
+  {
+    languageName: "java",
+    languageFileName: "java",
+  },
+];
 
-mycoding.forEach((item)=>{
-    const {languageName,languageFileName} = item
-    console.log(languageFileName,languageName);
-})
+mycoding.forEach((item) => {
+  const { languageName, languageFileName } = item;
+  console.log(languageFileName, languageName);
+});
