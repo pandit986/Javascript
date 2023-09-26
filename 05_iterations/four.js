179f34c0c74afe6771578b776fb21bed8e55b5dc
Abhishek Pandit <abhishekpandit986@gmail.com>
2023-09-26 12:05:19 +0000

    FOR IN LOOP

---
+++ 05_iterations/four.js
@@ -0,0 +1,29 @@
+const myProgramingLanguage = {
+    js:"javascript",
+    cpp:"c++",
+    rb:"ruby",
+    swift:"swift bt apple"
+}
+
+// for in Loop
+
+for (const key in myProgramingLanguage) {
+//    console.log(myProgramingLanguage[key]);
+}
+
+const programming = ["js","cpp","py","java"]
+
+for (const key in programming) {
+//    console.log(programming[key]);
+}
+
+
+//Map is not iterable
+// const map = new Map()
+// map.set("IN","India")
+// map.set("USA","United States of America")
+// map.set("FA","France")
+
+// for (const key in map) {
+//    console.log(key);
+// }
