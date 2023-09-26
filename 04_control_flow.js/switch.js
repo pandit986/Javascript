8793f131280eda29d3c79ba37462d50ba0a1ce1f
Abhishek Pandit <abhishekpandit986@gmail.com>
2023-09-26 05:39:13 +0000

    switch

---
+++ 04_control_flow.js/switch.js
@@ -0,0 +1,28 @@
+// multiple case check
+
+const month = 3;
+
+switch (month) {
+  case 1:
+    console.log("january");
+    break;
+  case 2:
+    console.log("feb");
+    break;
+  case 3:
+    console.log("march");
+    break;
+  case 4:
+    console.log("april");
+    break;
+  case 5:
+    console.log("may");
+    break;
+  default:
+    console.log("Not Found");
+    break;
+}
+
+//controll flow ko brack kar data
+
+//
